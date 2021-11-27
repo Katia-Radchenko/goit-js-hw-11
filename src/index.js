277c2c4740d3bd2a00314780d1dcca408d85422e
Katia-Radchenko <82673991+Katia-Radchenko@users.noreply.github.com>
2021-11-27 19:33:23 +0200

    правки ментора

--- src/index.js
+++ src/index.js
@@ -1,1 +1,103 @@
-import './sass/main.scss';
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImages, firstPage, imagesCount, RENDER_IMAGES_NUMBERS } from "./js/fetchImages";
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreButton: document.querySelector('.load-more'),
+};
+
+let pictureName = '';
+hideButton(refs.loadMoreButton);
+refs.form.addEventListener('submit', Search);
+refs.loadMoreButton.addEventListener('click', LoadMore);
+
+function Search(e) {
+  e.preventDefault();
+  pictureName = e.currentTarget.searchQuery.value;
+  firstPage();
+  hideButton(refs.loadMoreButton);
+  fetchImages(pictureName)
+    .then(images => {
+      const imagesArray = images.data.hits;
+      const totalImages = images.data.totalHits;
+
+      if (imagesArray.length === 0) {
+      return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.',);
+    } else {
+        clear();
+        markupGallery(imagesArray);
+        new SimpleLightbox('.gallery a', { captionDelay: 250, showCounter: false });
+      Notiflix.Notify.success(`Hooray! We found ${totalImages} images.`);
+      
+      if (totalImages <= RENDER_IMAGES_NUMBERS) {
+            return
+        }
+        showButton(refs.loadMoreButton);
+    }
+    });
+}
+
+function LoadMore() {
+  fetchImages(pictureName)
+    .then(images => {
+      const imagesArray = images.data.hits;
+      const totalImages = images.data.totalHits;
+      markupGallery(imagesArray);
+      new SimpleLightbox('.gallery a', { captionDelay: 250, showCounter: false });
+      if (imagesCount >= totalImages) {
+        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+        hideButton(refs.loadMoreButton);
+      }
+    })
+  
+    .catch(error => {
+      console.log(error);
+      hideButton(refs.loadMoreButton);
+    });
+}
+
+function markupGallery(images) {
+  const markup = images
+    .map(image => {
+      return `
+        <a class="gallery__item" href="${image.largeImageURL}">
+          <div class="photo-card">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item">
+                <b>Likes</b>
+                ${image.likes}
+              </p>
+              <p class="info-item">
+                <b>Views</b>
+                ${image.views}
+              </p>
+              <p class="info-item">
+                <b>Comments</b>
+                ${image.comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads</b>
+                ${image.downloads}
+              </p>
+            </div>
+          </div>
+        </a>`;
+    })
+    .join('');
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clear() {
+  refs.gallery.innerHTML = '';
+}
+function hideButton(item) {
+    item.classList.add('visually-hidden');
+}
+function showButton(item) {
+    item.classList.remove('visually-hidden');
+}
---
+++ src/js/fetchImages.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+export { fetchImages, firstPage };
+
+    
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = `24425918-1c58292ec38f4df582c31de2d`;
+export const RENDER_IMAGES_NUMBERS = 40
+let page = 1;
+export let imagesCount = 0;
+
+async function fetchImages(pictureName) {
+  const FULL_URL =
+    `${BASE_URL}?key=${API_KEY}&q=${pictureName}
+    &image_type=photo&orientation=horizontal&safesearch=true&page=${page}
+    &per_page=${RENDER_IMAGES_NUMBERS}`;
+  
+  try {
+    const images = await axios.get(FULL_URL);
+    page += 1;
+    // console.log(images);
+    
+    imagesCount += images.data.hits.length
+
+    return await images;  
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+function firstPage() {
+  page = 1;
+}
